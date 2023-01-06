68a9a1ac6f67ed8bfa472a6f3a8635b655b3e10e
Julian Piasecki <julian.piasecki@outlook.com>
2023-01-06 16:11:37 +0100

    New Features

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const taskListDiv = document.getElementById('taskList');
+/* Getting the elements from the HTML file. */
+const taskList = document.getElementById('taskList');
 const addTaskInput = document.getElementById('addTaskInput');
-const addTaskButton = document.getElementById('addTaskButton');
+const addTaskBtn = document.getElementById('addTaskButton');
 
+/* Creating an empty array. */
 let tasks = [];
 
-addTaskButton.onclick = () => createElement();
+/* Calling the function `createElement()` when the button is clicked. */
+addTaskBtn.onclick = () => createElement();
 
+/**
+ * It creates a new task and adds it to the list.
+ */
 const createElement = () => {
   tasks.push({
     id: tasks.length,
@@ -15,43 +21,61 @@ const createElement = () => {
 
   let element;
   let button;
+
   for (const task of tasks) {
     element = document.createElement('li');
     button = document.createElement('button');
 
     element.id = task.id;
     element.className = 'task';
-    element.innerText = `${task.name} | ${isDoneEmoji[task.isDone]}`;
+    element.innerText = `⟨${isDoneEmoji[task.isDone]}⟩ ${task.name}`;
     element.addEventListener('click', () => doneTask(task, element));
 
     button.id = task.id;
-    button.className = 'taskButton';
+    button.className = 'taskBtn';
     button.innerText = 'Delete task';
     button.addEventListener('click', () => removeTask(task, element, button));
   }
-  taskListDiv.appendChild(element);
-  taskListDiv.appendChild(button);
+  taskList.appendChild(element);
+  taskList.appendChild(button);
 };
 
+/* An object that contains the emojis that will be used to show if the task is done or not. */
 const isDoneEmoji = {
   true: '✅',
   false: '❌',
 };
 
+/**
+ * When the user clicks on a task, the task is marked as done and the text is crossed out.
+ * @param task - the task object
+ * @param element - the element that was clicked
+ */
 const doneTask = (task, element) => {
   task.isDone = true;
   element.style.textDecoration = 'line-through';
-  element.innerText = `${task.name} | ${isDoneEmoji[task.isDone]}`;
+  element.innerText = `⟨${isDoneEmoji[task.isDone]}⟩ ${task.name}`;
   element.addEventListener('click', () => undoneTask(task, element));
 };
 
+/**
+ * When the user clicks on a task, the task is undone and the text decoration is removed.
+ * @param task - the task object
+ * @param element - the element that was clicked
+ */
 const undoneTask = (task, element) => {
   task.isDone = false;
   element.style.textDecoration = '';
-  element.innerText = `${task.name} | ${isDoneEmoji[task.isDone]}`;
+  element.innerText = `⟨${isDoneEmoji[task.isDone]}⟩ ${task.name}`;
   element.addEventListener('click', () => doneTask(task, element));
 };
 
+/**
+ * It removes the task from the DOM and from the tasks array.
+ * @param task - the task object
+ * @param element - the element that contains the task
+ * @param button - the button that was clicked
+ */
 const removeTask = (task, element, button) => {
   element.parentNode.removeChild(element);
   button.parentNode.removeChild(button);
